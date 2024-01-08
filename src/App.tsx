bd39ddf40e043eca4726b143d70282e6cc5fa8c6
val131990 <val131990@outlook.com>
2024-01-09 00:51:02 +0100

    play with the server

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [response, setResponse] = useState('');
+
+  const myDummyObj:{ [key: string]: string } = {
+    0: "hello",
+    1: "good",
+    2: "lets"
+  };
+
+  function getRandomInt(max:number) {
+    return Math.floor(Math.random() * max);
+  }
+
+  useEffect(()=>{
+      const rand = getRandomInt(3);
+      
+      fetch(`http://localhost:8080/hello/${rand != 0 ? myDummyObj[rand]:""}`)
+      .then(response=>response.text())
+      .then(data=>{        
+        setResponse(data)
+      })
+  },[count])
+  
 
   return (
     <>
@@ -28,6 +50,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p style={{background:"green"}}>{response}</p>
     </>
   )
 }
